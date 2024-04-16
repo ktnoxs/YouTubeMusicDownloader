e4f67d66d3c187f8c096daed4d826f157cb83722
JangSeHun <westreed@naver.com>
2024-04-16 22:08:38 +0900

    feat: version 추가

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from colorama import Fore, Style, init
+from version import VERSION
 
 # 초기화 (Windows에서는 이 단계가 필요합니다)
 init()
@@ -7,13 +8,15 @@ logo = \
 f"""
 {Fore.GREEN}{Style.BRIGHT}=========================================================
 
-            {Fore.WHITE}YouTube Music Downloader
-            {Fore.CYAN}개발: 갈대
+            {Fore.WHITE}YouTube Music Downloader {VERSION}
+            {Fore.CYAN}Developer: 갈대
             
 {Fore.GREEN}========================================================={Style.RESET_ALL}
 프로그램을 종료하시려면, exit()를 입력하시거나 [X]버튼을 누르세요.
 "파일이름.txt"로 입력하면 txt 파일 안의 링크를 자동으로 받습니다.
 다운로드된 폴더를 여시려면 open()을 입력하세요.
+유튜브 링크를 입력하시면 자동으로 다운로드가 진행됩니다.
+노래 이름을 검색하듯이 입력하면 유튜브 검색결과 첫번째가 다운로드됩니다.
 """
 
 
