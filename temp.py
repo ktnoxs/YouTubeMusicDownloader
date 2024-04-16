992b67e4c5d47b022d41583b7087eda38b579a35
JangSeHun <westreed@naver.com>
2024-04-16 21:22:43 +0900

    feat: 임시파일을 일괄 삭제하는 기능

---
+++ temp.py
@@ -0,0 +1,11 @@
+import os
+
+from gui import print_
+
+
+def remove_temp():
+    for file_name in os.listdir("temp"):
+        try:
+            os.remove(f"temp/{file_name}")
+        except Exception as e:
+            print_(f"임시파일을 삭제하는데 오류가 발생했습니다. {e}")
